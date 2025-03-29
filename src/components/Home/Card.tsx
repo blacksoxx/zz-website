0ffc1b34336bf0952a2118f2e5531e7863e0d767
tarek-gritli <tarek.gritli@insat.ucar.tn>
2025-03-30 00:06:06 +0100

    Fix music players playing concurrently

--- src/components/Home/Card.tsx
+++ src/components/Home/Card.tsx
@@ -2,19 +2,35 @@ import MusicPlayer from "./MusicPlayer";
 import "./Home.css";
 
 type CardProps = {
+  id: string;
   title: string;
   description: string;
   soundPath: string;
   about: string;
+  activePlayer: string | null;
+  setActivePlayer: (player: string | null) => void;
 };
 
-const Card = ({ title, description, soundPath, about }: CardProps) => {
+const Card = ({
+  id,
+  title,
+  description,
+  soundPath,
+  about,
+  activePlayer,
+  setActivePlayer,
+}: CardProps) => {
   return (
     <div className="detail-card">
       <h3>{title}</h3>
       <p>{description}</p>
       {/* Audio Player */}
-      <MusicPlayer soundPath={soundPath} />
+      <MusicPlayer
+        soundPath={soundPath}
+        isActive={activePlayer === id}
+        onPlay={() => setActivePlayer(id)}
+        onPause={() => setActivePlayer(null)}
+      />
       <button className="about-button">{about}</button>
     </div>
   );
