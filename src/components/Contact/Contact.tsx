97b2bce44f6d8046afb47d47e66dd022974b1d8d
tarek-gritli <tarek.gritli@insat.ucar.tn>
2025-04-10 22:50:01 +0100

    fix home page cards, music page padding and contact page

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,15 +1,15 @@
-import React, { useEffect, useRef } from 'react';
-import './Contact.css';
-import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
-import { Helmet } from 'react-helmet';
-import '@fortawesome/fontawesome-free/css/all.css';
-import emailjs from '@emailjs/browser';
+import React, { useEffect, useRef } from "react";
+import "./Contact.css";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import { Helmet } from "react-helmet";
+import "@fortawesome/fontawesome-free/css/all.css";
+import emailjs from "@emailjs/browser";
 
 const Contact: React.FC = () => {
   const formRef = useRef<HTMLFormElement | null>(null);
   useEffect(() => {
-    const header = document.querySelector('.header-container') as HTMLElement;
+    const header = document.querySelector(".header-container") as HTMLElement;
     if (header) {
       header.style.backdropFilter = `blur(20px)`;
     }
@@ -23,52 +23,72 @@ const Contact: React.FC = () => {
     // Send email using EmailJS
     emailjs
       .sendForm(
-        'YOUR_SERVICE_ID', // Replace with your EmailJS Service ID
-        'YOUR_TEMPLATE_ID', // Replace with your EmailJS Template ID
+        "YOUR_SERVICE_ID", // Replace with your EmailJS Service ID
+        "YOUR_TEMPLATE_ID", // Replace with your EmailJS Template ID
         formRef.current,
-        'YOUR_PUBLIC_KEY' // Replace with your EmailJS Public Key
+        "YOUR_PUBLIC_KEY" // Replace with your EmailJS Public Key
       )
       .then(
         (result) => {
-          console.log('Email sent successfully:', result.text);
-          alert('Your message has been sent!');
+          console.log("Email sent successfully:", result.text);
+          alert("Your message has been sent!");
         },
         (error) => {
-          console.error('Failed to send email:', error.text);
-          alert('Failed to send your message. Please try again.');
+          console.error("Failed to send email:", error.text);
+          alert("Failed to send your message. Please try again.");
         }
       );
   };
 
   return (
-    <div
-      className="contact-container"
-      style={{
-        backgroundImage: "url('/violin.jpg')", // Path relative to the public folder
-        backgroundSize: 'cover',
-        backgroundPosition: 'bottom',
-        backgroundRepeat: 'no-repeat',
-      }}
-    >
-      <Helmet>
-        <title>Contact | Zied Zouari</title>
-      </Helmet>
-      <Header />
-      <main className="contact-content">
-        <div className="contact-form-section">
-          <h2>CONTACT</h2>
-          <h1>Let's get in touch</h1>
-          <form ref={formRef} onSubmit={handleSubmit} className="contact-form">
-            <input type="text" name="user_name" placeholder="Name" required />
-            <input type="email" name="user_email" placeholder="Email" required />
-            <input type="text" name="user_subject" placeholder="Subject" required />
-            <textarea name="message" placeholder="Message" required></textarea>
-            <button type="submit">Send Message</button>
-          </form>
-        </div>
-      </main>
+    <>
+      <div
+        className="contact-container"
+        style={{
+          backgroundImage: "url('/violin.jpg')", // Path relative to the public folder
+          backgroundSize: "cover",
+          backgroundPosition: "bottom",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+        <Helmet>
+          <title>Contact | Zied Zouari</title>
+        </Helmet>
+        <Header />
+        <main className="contact-content">
+          <div className="contact-form-section">
+            <h2>CONTACT</h2>
+            <h1>Let's get in touch</h1>
+            <form
+              ref={formRef}
+              onSubmit={handleSubmit}
+              className="contact-form"
+            >
+              <input type="text" name="user_name" placeholder="Name" required />
+              <input
+                type="email"
+                name="user_email"
+                placeholder="Email"
+                required
+              />
+              <input
+                type="text"
+                name="user_subject"
+                placeholder="Subject"
+                required
+              />
+              <textarea
+                name="message"
+                placeholder="Message"
+                required
+              ></textarea>
+              <button type="submit">Send Message</button>
+            </form>
+          </div>
+        </main>
+      </div>
       <Footer />
-    </div>
+    </>
   );
 };
 
